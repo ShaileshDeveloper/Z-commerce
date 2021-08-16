2459c43b6a020a3f129970fee37a310841de0a29
ShaileshDsce <74795973+ShaileshDeveloper@users.noreply.github.com>
2021-08-16 21:37:51 +0530

    feat cart-screen added with subtotal functionality

--- frontend/src/actions/cartAction.js
+++ frontend/src/actions/cartAction.js
@@ -2,17 +2,26 @@ import axios from 'axios'
 
 export const addToCart =(id , qty) => async(dispatch , getState) => {
     const {data} = await axios.get(`/products/${id}`)
+
     dispatch({
         type:'CART_ADD_ITEM',
         payload:{
             product:data._id,
             name:data.name,
             image:data.image,
-            price:data.proce,
+            price:data.price,
             countInStock:data.countInStock,
              qty
         }})
 
-localStorage.setItem('cartItems' , JSON.stringify(getState().addToCart.cartItems))
+localStorage.setItem('cartItems' , JSON.stringify(getState().cart.cartItems))
 
     }
+export const removeFromCart =(id) => (dispatch , getState) => {
+    dispatch({ 
+        type:"CART_REMOVE_ITEM", 
+        payload: id
+    })
+
+    localStorage.setItem('cartItems' , JSON.stringify(getState().cart.cartItems))
+}
--- frontend/src/screens/Productscreen.js
+++ frontend/src/screens/Productscreen.js
@@ -16,7 +16,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { productDetailsAction } from "../actions/productActions";
 
 function Productscreen({ history, match }) {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
 
   const dispatch = useDispatch();
   const productDetails = useSelector((state) => state.productDetails);
